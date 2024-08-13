c90d4fd408155a8c4995105eb987844465325ee5
herong <userherong@163.com>
2024-08-13 15:06:01 +0800

    添加项目基本路径解决404问题

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -5,6 +5,7 @@ import './style.css'
 
 /** @type {import('vitepress').Theme} */
 export default {
+  base:'my-web-site',
   extends: DefaultTheme,
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
